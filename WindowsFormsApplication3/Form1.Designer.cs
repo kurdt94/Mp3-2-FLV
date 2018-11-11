8a15f4dca81e48d6a1426d801bdbc75217ff09d4
THE REAL K94 . <kurdt94@gmail.com>
2018-11-11 04:42:54 +0100

    Add project files.

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submit = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // submit
+            // 
+            this.submit.Enabled = false;
+            this.submit.Location = new System.Drawing.Point(297, 144);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(132, 39);
+            this.submit.TabIndex = 0;
+            this.submit.Text = "convert";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(331, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Please select your image";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(140, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Image File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(140, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mp3 File";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(165, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(331, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Please select your audio file";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(121, 98);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Image Button
+            // 
+            this.button1.Location = new System.Drawing.Point(502, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Select Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.image_Click);
+            // 
+            // Audio Button
+            // 
+            this.button2.Location = new System.Drawing.Point(502, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Select Audio File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.audio_Click);
+            // 
+            // LOG
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 220);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(672, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // LOG LABEL
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Status";
+            // 
+            // FORM CONTROLS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 252);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.submit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+       
+
+        }
+
+        public Boolean audio_ready = false;
+        public Boolean image_ready = false;
+        public Boolean ready = false;
+   
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // SUMBMIT BUTTON CLICK 
+        private void submit_Click(object sender, EventArgs e)
+        {
+            // ffmpeg -r 1 -loop 1 -i image.jpg -i audio.mp3 -acodec copy -r 1 -shortest -vf scale=1280:720 ep1.flv
+
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = Application.StartupPath + @"\common\ffmpeg.exe";
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.Arguments = $"-r 1 -loop 1 -i \"{textBox1.Text}\" -i \"{textBox2.Text}\" -acodec copy -r 1 -shortest -vf scale=1280:720 \"{System.IO.Path.ChangeExtension(textBox2.Text, null)}.flv\"";
+
+            textBox3.Text = startInfo.Arguments;
+           
+
+            try
+            {
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+
+                    exeProcess.WaitForExit();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        
+
+    }
+
+        // Select Image File
+        private void image_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "button1_Click_1";
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Image Files|*.jpg";
+
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    FileInfo fileinfo = new FileInfo(file);
+                    long fsize = fileinfo.Length;
+
+                    textBox1.Text = file;
+                    pictureBox1.Image = Image.FromFile(file);
+
+                    label1.Text = Decimal.Round(fsize, 2).ToString();  
+
+                }
+                catch (System.IO.IOException)
+                {
+                }
+            }
+
+        }
+        
+        // Select AUDIO FILE
+        private void audio_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            textBox3.Text = "button2_Click";
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Mp3 Audio Files|*.mp3";
+
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    FileInfo fileinfo = new FileInfo(file);
+                    long fsize = fileinfo.Length;
+
+                    textBox2.Text = file;
+                    label2.Text = Decimal.Round(fsize, 2).ToString();
+
+                }
+                catch (System.IO.IOException)
+                {
+                }
+            }
+            Console.WriteLine(size); // <-- Shows file size in debugging mode.
+            Console.WriteLine(result); // <-- For debugging use.
+        }
+        
+        // On Change AUDIO file
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (File.Exists(textBox2.Text))
+            {
+                this.audio_ready = true;
+            }
+            else {
+                this.audio_ready = false;
+                return;
+            }
+
+            if (this.audio_ready == true && this.image_ready == true) {
+                submit.Enabled = true;
+            }
+
+        }
+
+        // On Change IMAGE file
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (File.Exists(textBox1.Text)){
+                this.image_ready = true;
+            }
+            else
+            {
+                this.image_ready = false;
+                return;
+            }
+        }
+
+
+    }
+
+
+
+}
